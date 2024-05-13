1de8e79f9d256024943dd45a1534397054301fd6
Sugoto <sugotobasu1@gmail.com>
2024-05-13 09:23:26 +0530

    Update ApiGatewayRouteTest.java

--- api-gateway/src/test/java/ApiGatewayRouteTest.java
+++ api-gateway/src/test/java/ApiGatewayRouteTest.java
@@ -1,5 +1,3 @@
-package br.com.springboot.netflix.gateway;
-
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
